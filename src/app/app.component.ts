aefc4c1ba553fbe520d9ebf3f7cfd1d087352cd5
haimei.zhang <haimeizhang2015@gmail.com>
2020-03-11 21:08:37 +0000

    US_PSW-8648 CLONE - Mosaic Dashboard - UI
    add mosaic framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dashboard-ui-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'cmp-angular-dashboard-ui';
+
+  sidebarToggled = false;
+
+  toggleSidebar(): void {
+    this.sidebarToggled = !this.sidebarToggled;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MosaicIconsComponent } from '~components/mosaic-icons/mosaic-icons.component';
+
+import { HomeComponent } from '~views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MosaicIconsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
